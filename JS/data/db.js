70ac54ae1f38994280a58e3e3747cb19a6ee1d17
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ JS/data/db.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    var url = "http://localhost:59611/Categories";
+
+    Application1.db = DevExpress.data.AspNet.createStore({
+        key: "CategoryID",
+        loadUrl: url,
+        insertUrl: url,
+        updateUrl: url,
+        deleteUrl: url
+    });
+})()
---
+++ JS/views/home.js
@@ -0,0 +1,78 @@
+﻿Application1.home = function (params) {
+    var viewModel = {
+        gridSettings: {
+            dataSource: { store: Application1.db },
+            height: 800,
+            columns: [{
+                dataField: "CategoryID",
+                dataType: "number",
+                allowEditing: false
+            }, {
+                dataField: "CategoryName"
+            }, {
+                dataField: "RegisterDate",
+                dataType: "date"
+            }, {
+                dataField: "Count",
+                dataType: "number"
+            }, {
+                dataField: "Price",
+                format: "fixedPoint",
+                precision: 2,
+                dataType: "number"
+            }],
+            groupPanel: {
+                visible: true
+            },
+            sorting: {
+                mode: "multiple"
+            },
+            searchPanel: {
+                visible: true
+            },
+            filterRow: {
+                visible: true
+            },
+            editing: {
+                mode: "batch",
+                allowAdding: true,
+                allowUpdating: true,
+                allowDeleting: true
+            },
+            grouping: {
+                autoExpandAll: false
+            },
+            pager: {
+                showPageSizeSelector: true,
+                showInfo: true
+            },
+            summary: {
+                totalItems: [{
+                    column: "Count",
+                    summaryType: "sum"
+                }, {
+                    column: "Price",
+                    summaryType: "avg"
+                }, {
+                    column: "CategoryID",
+                    summaryType: "count"
+                }],
+                groupItems: [{
+                    column: "Count",
+                    summaryType: "sum"
+                }, {
+                    column: "Price",
+                    summaryType: "avg"
+                }, {
+                    summaryType: "count"
+                }]
+            },
+            remoteOperations: true,
+            headerFilter: {
+                visible: true
+            }
+        }
+    };
+
+    return viewModel;
+};
